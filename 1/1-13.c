3ae4c08309cb64a9a2edd2d5c7469b93cde7ab8d
malan88 <malan88@standingwater.io>
2018-08-12 14:16:41 -0400

    Ex 1-13

---
+++ 1/1-13.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define IN  1   /* inside a word */
+#define OUT 0   /* outside a word */
+
+/* count lines, words, and characters in input */
+main()
+{
+    int c, nw, state;
+    int wl[1000];
+
+    for (int i = 0; i < 1000; ++i)
+        wl[i] = 0;
+
+    state = OUT;
+    nw = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+            wl[nw]++;
+        }else
+            wl[nw]++;
+    }
+    
+    for (nw = 0; nw < 1000; ++nw)
+        if (wl[nw] > 0){
+            printf("%3d: ", nw);
+            for (int i = 0; i < wl[nw]; i++)
+                putchar('%');
+            putchar('\n');
+        }
+}
